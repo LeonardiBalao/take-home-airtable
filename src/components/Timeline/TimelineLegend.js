0d36c293667958fe286e1fce727333ebbb122f9f
Matheus <balaolmatheus@gmail.com>
2025-08-06 21:33:25 -0300

    Final version

--- src/components/Timeline/TimelineLegend.js
+++ src/components/Timeline/TimelineLegend.js
@@ -3,7 +3,7 @@ import React from "react";
 const TimelineLegend = () => {
   return (
     <div className="mt-4 text-sm text-gray-600">
-      <p>Double-click any item to edit its name. Use the zoom slider to adjust the timeline scale.</p>
+      <p>Double-click any item to edit its name. Use the zoom slider or the mouse scroll to adjust the timeline scale. (desktop)</p>
     </div>
   );
 };
